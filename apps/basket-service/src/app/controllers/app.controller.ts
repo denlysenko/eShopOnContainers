f69f36b1377ae818b3b93098ceadcfa623e090e1
denlysenko <den.lysenko.mail@gmail.com>
2021-11-17 23:08:34 +0200

    basket: add controller

---
+++ apps/basket-service/src/app/controllers/app.controller.ts
@@ -0,0 +1,107 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  ParseArrayPipe,
+  Post,
+} from '@nestjs/common';
+import { ApiOAuth2, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import {
+  AppService,
+  BasketCheckoutDto,
+  BasketItemCreateDto,
+  CustomerBasketReadDto,
+  ValidationErrorDto,
+} from '../application';
+import { Identity } from '../infrastructure';
+
+@Controller('v1/basket')
+export class AppController {
+  constructor(private readonly _appService: AppService) {}
+
+  // GET api/v1/basket
+  @Get()
+  @ApiOperation({ summary: 'Get customer basket by customerId' })
+  @ApiOAuth2([])
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Found record',
+    type: CustomerBasketReadDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Not Found Error',
+  })
+  getBasketById(
+    @Identity() customerId: string
+  ): Promise<CustomerBasketReadDto> {
+    return this._appService.getBasketById(customerId);
+  }
+
+  // POST api/v1/basket
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Creates/updates customer basket' })
+  @ApiOAuth2([])
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Created/update record',
+    type: CustomerBasketReadDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNPROCESSABLE_ENTITY,
+    description: 'Validation Error',
+    type: ValidationErrorDto,
+  })
+  updateBasket(
+    @Identity() customerId: string,
+    @Body(new ParseArrayPipe({ items: BasketItemCreateDto }))
+    basketItemsDto: BasketItemCreateDto[]
+  ): Promise<CustomerBasketReadDto> {
+    return this._appService.updateBasket(customerId, basketItemsDto);
+  }
+
+  // POST api/v1/basket/checkout
+  @Post('checkout')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Checkout customer order' })
+  @ApiOAuth2([])
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Order checked out',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Not Found Error',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNPROCESSABLE_ENTITY,
+    description: 'Validation Error',
+    type: ValidationErrorDto,
+  })
+  checkout(
+    @Identity() customerId: string,
+    @Body() basketCheckoutDto: BasketCheckoutDto
+  ): Promise<void> {
+    return this._appService.checkout(customerId, basketCheckoutDto);
+  }
+
+  // DELETE api/v1/basket
+  @Delete()
+  @ApiOperation({ summary: 'Deletes customer basket' })
+  @ApiOAuth2([])
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Record deleted',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Not Found Error',
+  })
+  deleteBasket(@Identity() customerId: string): Promise<void> {
+    return this._appService.deleteBasket(customerId);
+  }
+}
