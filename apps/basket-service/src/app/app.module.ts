7579897201b999a6de0600b2cd4a6b02b136da91
denlysenko <den.lysenko.mail@gmail.com>
2021-11-19 17:12:02 +0200

    basket: provide dependencies

--- apps/basket-service/src/app/app.module.ts
+++ apps/basket-service/src/app/app.module.ts
@@ -1,8 +1,33 @@
-import { Module } from '@nestjs/common';
+import {
+  EventBusClient,
+  EVENT_BUS_CLIENT,
+} from '@e-shop-on-containers/event-bus';
+import { ILogger, LOGGER } from '@e-shop-on-containers/logger';
+import { Logger, Module } from '@nestjs/common';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppService } from './application';
+import {
+  AppService,
+  BasketRepository,
+  BASKET_REPOSITORY,
+  OutboxRepository,
+  OutboxService,
+  OUTBOX_REPOSITORY,
+  UnitOfWork,
+} from './application';
 import { AppController } from './controllers';
-import { DatabaseModule } from './infrastructure';
+import {
+  DatabaseModule,
+  MessageProcessor,
+  ProductPriceChangedConsumer,
+  RbmqEventBusClient,
+  RBMQ_MESSAGE_BUS_CLIENT,
+  TypeOrmUnitOfWork,
+} from './infrastructure';
+
+export const queue = process.env.EVENT_QUEUE_NAME;
+export const eventBusConnection =
+  process.env.EVENT_BUS_CONNECTION || 'amqp://localhost:5672';
 
 @Module({
   imports: [
@@ -14,7 +39,49 @@ import { DatabaseModule } from './infrastructure';
     }),
     DatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProductPriceChangedConsumer],
+  providers: [
+    MessageProcessor,
+    {
+      provide: EVENT_BUS_CLIENT,
+      useClass: RbmqEventBusClient,
+    },
+    {
+      provide: RBMQ_MESSAGE_BUS_CLIENT,
+      useFactory: () =>
+        ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            urls: [eventBusConnection],
+            queue,
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+    },
+    {
+      provide: LOGGER,
+      useClass: Logger,
+    },
+    {
+      provide: OutboxService,
+      useFactory: (
+        eventBusClient: EventBusClient,
+        outboxRepository: OutboxRepository,
+        logger: ILogger
+      ) => new OutboxService(eventBusClient, outboxRepository, logger),
+      inject: [EVENT_BUS_CLIENT, OUTBOX_REPOSITORY, LOGGER],
+    },
+    {
+      provide: AppService,
+      useFactory: (
+        basketRepository: BasketRepository,
+        outboxRepository: OutboxRepository,
+        unitOfWork: UnitOfWork
+      ) => new AppService(basketRepository, outboxRepository, unitOfWork),
+      inject: [BASKET_REPOSITORY, OUTBOX_REPOSITORY, TypeOrmUnitOfWork],
+    },
+  ],
 })
 export class AppModule {}
--- apps/basket-service/src/app/infrastructure/index.ts
+++ apps/basket-service/src/app/infrastructure/index.ts
@@ -1,4 +1,5 @@
 export * from './consumers/product-price-changed.consumer';
 export * from './database/database.module';
+export * from './database/typeorm-unit-of-work';
 export * from './message-processor/message.processor';
 export * from './rbmq-event-bus/rbmq-event-bus.client';
