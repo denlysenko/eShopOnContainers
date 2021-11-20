3ac699c5d81f8fb268c301d36f2ba1acc046ec7e
denlysenko <den.lysenko.mail@gmail.com>
2021-11-20 20:36:32 +0200

    web-shopping: generate app

---
+++ apps/web-shopping/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'e-shop-on-containers-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web-shopping';
+}
