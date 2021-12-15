759a3a600b1793a878e248355a7803d13b3f9ab3
denlysenko <den.lysenko.mail@gmail.com>
2021-12-15 22:23:26 +0200

    [identity service] enable health check

--- apps/web-shopping-bff/src/app/health.controller.ts
+++ apps/web-shopping-bff/src/app/health.controller.ts
@@ -16,9 +16,26 @@ export class HealthController {
   @HealthCheck()
   check() {
     return this._health.check([
-      () => this._http.pingCheck('basket-service', `${process.env.BASKET_URL}/health`),
-      () => this._http.pingCheck('catalog-service', `${process.env.CATALOG_URL}/health`),
-      () => this._http.pingCheck('ordering-service', `${process.env.ORDERING_URL}/health`),
+      () =>
+        this._http.pingCheck(
+          'basket-service',
+          `${process.env.BASKET_URL}/health`
+        ),
+      () =>
+        this._http.pingCheck(
+          'catalog-service',
+          `${process.env.CATALOG_URL}/health`
+        ),
+      () =>
+        this._http.pingCheck(
+          'ordering-service',
+          `${process.env.ORDERING_URL}/health`
+        ),
+      () =>
+        this._http.pingCheck(
+          'identity-service',
+          process.env.KEYCLOAK_HEALTH_CHECK_URL
+        ),
     ]);
   }
 }
